c1a12e885491000de2085704856f74614c1531ca
priximmo <xavier.pestel@gmail.com>
2018-10-09 17:26:26 +0200

    Update wp-config.php

--- html/wp-config.php
+++ html/wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'blog');
 define('DB_USER', 'newuser');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password');
+define('DB_PASSWORD', 'xxxxxx');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
